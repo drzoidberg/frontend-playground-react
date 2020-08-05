00cd5ee37cbfb687f0a8c41294385f8833cdf43f
Jorge Vicens Valor <gromit.jvv@gmail.com>
2020-08-05 11:24:46 +0200

    🚧 fixed dependency of Template component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import Wrapper from './components/UI/Wrapper/Wrapper';
 import Parallax from './components/UI/Parallax/Parallax';
 import JsTextAnimation from './components/UI/JsTextAnimation/JsTextAnimation';
-import Dummy from './components/Dummy/Dummy';
+import Template from './components/Template/Template';
 
 import classes from './App.module.css'
 
